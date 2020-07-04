d5528d6e0e11a8dbfc18b492e2620c3ef99b29fd
SyedIftekharuddin <syedy636@gmail.com>
2020-07-04 20:00:48 +0200

    Initial Project Add

---
+++ src/main/java/com/app/utb/sfgpetclinicspringboot2/SfgPetClinicSpringboot2Application.java
@@ -0,0 +1,13 @@
+package com.app.utb.sfgpetclinicspringboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicSpringboot2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinicSpringboot2Application.class, args);
+    }
+
+}
